470b493ecb668c003887f024ae9c4f4186dddb7b
choideakook <shdrnrhd113@naver.com>
2023-10-19 19:07:02 +0900

    feat: mission id 로 코드리뷰 목록 조회 sql 구현

--- src/main/java/com/baeker/Community/category/adapter/out/persistence/CodeReviewQueryRepository.java
+++ src/main/java/com/baeker/Community/category/adapter/out/persistence/CodeReviewQueryRepository.java
@@ -1,21 +1,66 @@
 package com.baeker.Community.category.adapter.out.persistence;
 
 import com.baeker.Community.category.application.prot.out.CodeReviewQueryRepositoryPort;
+import com.baeker.Community.category.domain.QCodeReview;
+import com.baeker.Community.comment.domain.QComment;
 import com.baeker.Community.global.dto.resDto.ChallengerDto;
+import com.baeker.Community.global.dto.resDto.CodeReviewDto;
+import com.baeker.Community.global.dto.resDto.QChallengerDto;
+import com.baeker.Community.global.dto.resDto.QCodeReviewDto;
+import com.baeker.Community.post.domain.QPost;
+import com.querydsl.core.Tuple;
+import com.querydsl.core.types.Expression;
+import com.querydsl.core.types.ExpressionUtils;
+import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Repository
 @RequiredArgsConstructor
 public class CodeReviewQueryRepository implements CodeReviewQueryRepositoryPort {
 
     private final JPAQueryFactory query;
+    private QCodeReview cr = QCodeReview.codeReview;
+    private QPost p = QPost.post;
 
     @Override
     public List<ChallengerDto> findByMissionId(Long missionId) {
-        return null;
+
+        List<Tuple> result = query
+                .select(p.memberId, codeReviewDto())
+                .from(cr)
+                .join(cr.post, p)
+                .where(cr.missionId.eq(missionId))
+                .groupBy(p.memberId)
+                .fetch();
+
+        return result.stream()
+                .map(tuple -> {
+                    Long memberId = tuple.get(p.memberId);
+                    CodeReviewDto codeReview = tuple.get(codeReviewDto());
+                    return new ChallengerDto(memberId, Collections.singletonList(codeReview));
+                })
+                .collect(Collectors.toList());
+    }
+
+
+
+    private QCodeReviewDto codeReviewDto() {
+        return new QCodeReviewDto(
+                p.id,
+                p.memberId,
+                cr.problemStatusId,
+                p.createDate,
+                p.pageView,
+                p.followList.size(),
+                p.title,
+                p.content,
+                p.commentList
+        );
     }
 }
