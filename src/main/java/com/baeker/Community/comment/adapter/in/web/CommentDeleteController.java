6b5f7ee3cc457d707b1c88993c46cd5253ccf87b
choideakook <shdrnrhd113@naver.com>
2023-10-26 17:46:46 +0900

    feat: 댓글 삭제 api 구현

--- src/main/java/com/baeker/Community/comment/adapter/in/web/CommentDeleteController.java
+++ src/main/java/com/baeker/Community/comment/adapter/in/web/CommentDeleteController.java
@@ -2,10 +2,13 @@ package com.baeker.Community.comment.adapter.in.web;
 
 import com.baeker.Community.comment.application.port.in.CommentDeleteUseCase;
 import com.baeker.Community.comment.application.port.in.CommentQueryUseCase;
+import com.baeker.Community.comment.domain.Comment;
+import com.baeker.Community.global.jwt.JwtDecrypt;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "COMMENT")
 @RestController
@@ -15,4 +18,18 @@ public class CommentDeleteController {
 
     private final CommentQueryUseCase commentQueryUseCase;
     private final CommentDeleteUseCase commentDeleteUseCase;
+    private final JwtDecrypt decrypt;
+
+
+    @Operation(summary = "댓글 삭제")
+    @DeleteMapping("/v1/{commentId}")
+    public ResponseEntity deleteComment(
+            @RequestHeader("Authorization") String token,
+            @PathVariable Long commentId
+    ) {
+        Long memberId = decrypt.getMemberId(token);
+        Comment comment = commentQueryUseCase.byId(commentId);
+        commentDeleteUseCase.comment(memberId, comment);
+        return ResponseEntity.noContent().build();
+    }
 }
