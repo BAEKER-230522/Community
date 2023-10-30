40c74f22e2eafc97d9fef8204fa7fc3dbf829fbc
choideakook <shdrnrhd113@naver.com>
2023-10-30 15:31:02 +0900

    feat: 정회원 여부 확인 요청 포트 구현

--- src/main/java/com/baeker/Community/post/application/port/out/feign/StudyClientPort.java
+++ src/main/java/com/baeker/Community/post/application/port/out/feign/StudyClientPort.java
@@ -2,7 +2,17 @@ package com.baeker.Community.post.application.port.out.feign;
 
 import com.baeker.Community.global.appConfig.FeignConfiguration;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "study", url = "${custom.feign.study}", configuration = FeignConfiguration.class)
 public interface StudyClientPort {
+
+
+    @GetMapping("/v1/member/{memberId}/{studyId}")
+    ResponseEntity isMember(
+            @PathVariable("memberId") Long memberId,
+            @PathVariable("studyId") Long studyId
+    );
 }
