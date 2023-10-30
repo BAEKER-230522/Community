340852c727b4cb2a6e9b4734f9da76ea4fa7af8a
choideakook <shdrnrhd113@naver.com>
2023-10-30 21:12:29 +0900

    test: ResultActions 오류 변수화 안되는 문제 발생

--- src/test/java/com/baeker/Community/post/adapter/in/create/PostCreateController_codeReviewTest.java
+++ src/test/java/com/baeker/Community/post/adapter/in/create/PostCreateController_codeReviewTest.java
@@ -4,6 +4,8 @@ import com.baeker.Community.global.dto.reqDto.CreateCodeReviewDto;
 import com.baeker.Community.global.dto.resDto.CodeReviewDto;
 import com.baeker.Community.global.testUtil.MockMvcRequest;
 import com.baeker.Community.global.testUtil.TestData;
+import com.baeker.Community.post.adapter.in.requestMock.ApiStudyClientMock;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,27 +17,34 @@ import org.springframework.transaction.annotation.Transactional;
 
 import static com.baeker.Community.global.testUtil.MockMvcRequest.post;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @DisplayName("통합 - 코드리뷰 게시물 작성")
 @SpringBootTest
 @Transactional
 @AutoConfigureMockMvc
-class PostCreateController_codeReviewTest extends TestData {
+class PostCreateController_codeReviewTest extends ApiStudyClientMock {
 
     @Autowired MockMvc mvc;
+
+    @BeforeEach
+    void setup() {
+        memberCheckMocking();
+    }
 
 
     @Test
     @DisplayName("코드리뷰 게시물 등록 성공")
     void no1() throws Exception{
         Long
+                studyId = 1L,
                 missionId = 1L,
                 problemStatusId = 1L;
         String
                 title = "code review",
                 content = "hello";
-        CreateCodeReviewDto dto = toDto(missionId, problemStatusId, title, content);
+        CreateCodeReviewDto dto = toDto(studyId, missionId, problemStatusId, title, content);
 
 
         ResultActions result = post(mvc, POST_USER_URL +
@@ -50,7 +59,30 @@ class PostCreateController_codeReviewTest extends TestData {
         assertThat(resDto.getProblemStatusId()).isEqualTo(problemStatusId);
     }
 
-    private CreateCodeReviewDto toDto(Long missionId, Long problemStatusId, String title, String content) {
-        return new CreateCodeReviewDto(1L, missionId, problemStatusId, title, content);
+    @Test
+    @DisplayName("이미 게시물을 등록한 경우")
+    void n02() throws Exception {
+        Long problemStatusId = 1L;
+        CreateCodeReviewDto dto = toDto(problemStatusId);
+        post(mvc, POST_USER_URL + "/v1/code-review", jwt1, dto);
+
+
+        ResultActions result = post(mvc, POST_USER_URL +
+                "/v1/code-review", jwt1, dto);
+
+
+        result
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("errorMsg").value("이미 등록된 게시물"));
+    }
+
+
+
+    private CreateCodeReviewDto toDto(Long studyId, Long missionId, Long problemStatusId, String title, String content) {
+        return new CreateCodeReviewDto(studyId, missionId, problemStatusId, title, content);
+    }
+
+    private CreateCodeReviewDto toDto(Long problemStatusId) {
+        return new CreateCodeReviewDto(1L, 1L, problemStatusId, "title", "content");
     }
 }
